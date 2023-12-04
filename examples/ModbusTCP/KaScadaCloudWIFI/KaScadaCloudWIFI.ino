72eba1a924d9ab0dc792c85560c3b21f2aff2111
Totuin <117037443+Totuin@users.noreply.github.com>
2023-12-04 17:25:40 +0500

    Update KaScadaCloudWIFI.ino

--- examples/ModbusTCP/KaScadaCloudWIFI/KaScadaCloudWIFI.ino
+++ examples/ModbusTCP/KaScadaCloudWIFI/KaScadaCloudWIFI.ino
@@ -1,47 +1,108 @@
 // Подключаем необходимую библиотеку
 #include "flprogModbusTCP.h"
 
-FLProgOnBoardWifi WifiInterface;
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
 
-//Создаем объект непосредстредственно Модбас слейв на необходимом интерфейсе
+/*
+  -------------------------------------------------------------------------------------------------
+        Создание интерфейса для работы с Wifi интерфейсом
+
+  -------------------------------------------------------------------------------------------------
+*/
+FLProgOnBoardWifiInterface WifiInterface;
+
+
+
+/*
+  -----------------------------------------------------------------------------------------
+     Создаем объект непосредстредственно Модбас слейва на необходимом интерфейсе
+  -----------------------------------------------------------------------------------------
+*/
+
 ModbusKaScadaCloud Slave1(&WifiInterface);
 
-//Вспомогательные переменные для демонстрации
+/*
+  -----------------------------------------------------------------------------------------
+          Определение рабочих параметров и функций
+  -----------------------------------------------------------------------------------------
+*/
 int tempInt;
 unsigned long tempUnsigned;
+uint32_t blinkStartTime = 0;
+uint8_t ethernetStatus = 255;
+uint8_t ethernetError = 255;
 
+bool isNeedClientSendConnectMessage = true;
+bool isNeedClientSendDisconnectMessage = true;
+
+bool isNeedApSendConnectMessage = true;
+bool isNeedApSendDisconnectMessage = true;
+
+//=================================================================================================
 void setup()
 {
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  flprog::printConsole(" Тест Modbus KaScada Cloud - WIFI ");
+
+  /*
+    -----------------------------------------------------------------------------------------
+        Настройка интерфейса
+    -----------------------------------------------------------------------------------------
+  */
 
   WifiInterface.clientOn();
   WifiInterface.mac(0x78, 0xAC, 0xC0, 0x2C, 0x3E, 0x28);
-  WifiInterface.setClientSsidd("yana");
+  WifiInterface.setClientSsidd("totuin-router");
   WifiInterface.setClientPassword("12345678");
   // Привязываем слейв к TCP устройству
 
-  //Инициализация вспомогательных переменных
+  /*
+      -----------------------------------------------------------------------------------------
+          Инициализация вспомогательных переменных
+      -----------------------------------------------------------------------------------------
+  */
   tempUnsigned = millis() - 1000;
 
-  // Описываем таблицы слейва
+  /*
+      -----------------------------------------------------------------------------------------
+            Описываем таблицы слейва
+      -----------------------------------------------------------------------------------------
+  */
   Slave1.configDataTable(FLPROG_HOLDING_REGISTR, 10);
   Slave1.configDataTable(FLPROG_INPUT_REGISTR, 10);
   Slave1.configDataTable(FLPROG_COIL, 10, 5);
   Slave1.configDataTable(FLPROG_DISCRETE_INPUT, 10);
+
   /*
-    Задаём имя устройства KaScadaCloud.
+    -----------------------------------------------------------------------------------------
+      Задаём имя устройства KaScadaCloud.
     Вызов обязателен и так же может быть изменён в любой момент исполнения программы
+    -----------------------------------------------------------------------------------------
   */
   Slave1.setKaScadaCloudDevceId("DKK6T9TPE4");
 }
-
+//=================================================================================================
 void loop()
 {
-  // Цикл работы интерфейса
-  WifiInterface.pool();
-  //Цикл работы слейва
-  Slave1.pool();
 
-  //Демонстрационная логика - раз в секунду изменяем значение переменной.
+  WifiInterface.pool(); // Цикл работы интерфейса
+  Slave1.pool();          // Цикл работы слейва
+  printStatusMessages();
+  blinkLed();
+  workModbus();
+}
+
+//=================================================================================================
+void workModbus()
+{
+  // Демонстрационная логика - раз в секунду изменяем значение переменной.
 
   if (flprog::isTimer(tempUnsigned, 1000))
   {
@@ -73,4 +134,129 @@ void loop()
     Дополнительные функции
     byte getLastError(); - получение кода последней ошибки слейва
   */
+}
+
+
+void blinkLed()
+{
+  if (flprog::isTimer(blinkStartTime, 50))
+  {
+    blinkStartTime = millis();
+    digitalWrite(LED_BUILTIN, !(digitalRead(LED_BUILTIN)));
+  }
+}
+
+void printStatusMessages()
+{
+  if (WifiInterface.getStatus() != ethernetStatus)
+  {
+    ethernetStatus = WifiInterface.getStatus();
+    Serial.println();
+    Serial.print("Статус интерфейса - ");
+    Serial.println(flprog::flprogStatusCodeName(ethernetStatus));
+  }
+  if (WifiInterface.getError() != ethernetError)
+  {
+    ethernetError = WifiInterface.getError();
+    if (ethernetError != FLPROG_NOT_ERROR)
+    {
+      Serial.println();
+      Serial.print("Ошибка интерфейса - ");
+      Serial.println(flprog::flprogErrorCodeName(ethernetError));
+    }
+  }
+  printClientConnectMessages();
+  printClientDisconnectMessages();
+  printApConnectMessages();
+  printApDisconnectMessages();
+}
+
+void printClientConnectMessages()
+{
+  if (!WifiInterface.clientIsReady())
+  {
+    return;
+  }
+  if (!isNeedClientSendConnectMessage)
+  {
+    return;
+  }
+  Serial.println("WiFiClient подключён!");
+  Serial.print("Ssid - ");
+  Serial.println(WifiInterface.clientSsid());
+  Serial.print("Ip - ");
+  Serial.println(WifiInterface.localIP());
+  Serial.print("DNS - ");
+  Serial.println(WifiInterface.dns());
+  Serial.print("Subnet - ");
+  Serial.println(WifiInterface.subnet());
+  Serial.print("Gateway - ");
+  Serial.println(WifiInterface.gateway());
+  Serial.println();
+  isNeedClientSendConnectMessage = false;
+  isNeedClientSendDisconnectMessage = true;
+}
+
+void printApConnectMessages()
+{
+  if (!WifiInterface.apIsReady())
+  {
+    return;
+  }
+  if (!isNeedApSendConnectMessage)
+  {
+    return;
+  }
+  Serial.println("WiFi точка включенна!");
+  Serial.print("Ssid - ");
+  Serial.println(WifiInterface.apSsid());
+  Serial.print("Ip - ");
+  Serial.println(WifiInterface.apLocalIP());
+  Serial.print("DNS - ");
+  Serial.println(WifiInterface.apDns());
+  Serial.print("Subnet - ");
+  Serial.println(WifiInterface.apSubnet());
+  Serial.print("Gateway - ");
+  Serial.println(WifiInterface.apGateway());
+  Serial.println();
+  isNeedApSendConnectMessage = false;
+  isNeedApSendDisconnectMessage = true;
+}
+
+void printClientDisconnectMessages()
+{
+  if (WifiInterface.clientIsReady())
+  {
+    return;
+  }
+  if (isNeedClientSendConnectMessage)
+  {
+    return;
+  }
+  if (!isNeedClientSendDisconnectMessage)
+  {
+    return;
+  }
+  Serial.println("WiFiClient отключён!");
+  isNeedClientSendConnectMessage = true;
+  isNeedClientSendDisconnectMessage = false;
+}
+
+void printApDisconnectMessages()
+{
+  if (WifiInterface.apIsReady())
+  {
+    return;
+  }
+  if (isNeedApSendConnectMessage)
+  {
+    return;
+  }
+  if (!isNeedApSendDisconnectMessage)
+  {
+    return;
+  }
+  Serial.println("WiFi точка отключёна!");
+  isNeedApSendConnectMessage = true;
+  isNeedApSendDisconnectMessage = false;
 }
